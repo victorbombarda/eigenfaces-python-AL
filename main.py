6987476d6badfed5324ff1c3f7912f98a2249f23
Victor <70376108+victorbombarda@users.noreply.github.com>
2020-11-17 16:19:55 -0300

    Imagens formadas e média agora com uint8
    Antes tinhamos problema nos cálculos pois não estava tudo com o mesmo tipo (era numpy.float64 para a média e uint8 para as imagens). Agora todos estão no mesmo tipo e podemos visualizar as imagens normalmente.

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 import glob
 import cv2
 import numpy as np
+from skimage import img_as_float, img_as_ubyte
+from PIL import Image
 
 
 def load_images(directory):
 	# get a list of all the picture filenames
 	jpgs = glob.glob(directory + '/*.jpeg')
 	# load a greyscale version of each image
-	imgs = np.array([cv2.imread(i, 1).flatten() for i in jpgs])
+	imgs = np.array([cv2.imread(i, 0).flatten() for i in jpgs])
 	return imgs
 
 #Carregamos as fotos em uma variável
 images = load_images(r"faces_jpeg")
+images_teste = load_images(r"faces_jpeg")
 
 #Criando o vetor média
 mean = np.zeros(images[0].shape[0])
 for i in images:
     mean = mean + i
 mean = mean * 1/100
+#Transformamos do float64 para  grayscale
+mean = mean.astype(np.uint8)
+
+#Visualizamos a imagem média
+mean_img = mean.reshape(983,800)
+Image.fromarray(mean_img)
 
 #Diminuímos cada coluna da matriz do vetor média
-images_transf = images
 for i in range(len(images)):
-    images_transf[i] = images[i] - mean
+    images_teste[i] = images[i] - mean
 
+#Conseguimos visualizar as imagens normalizadas
+teste_img = images_teste[0].reshape(983,800)
+Image.fromarray(teste_img)
