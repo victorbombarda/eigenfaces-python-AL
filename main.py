721963c6cfbaef75c6ff84ef08e8c51f0d394acd
Victor <70376108+victorbombarda@users.noreply.github.com>
2020-11-17 12:26:29 -0300

    Update main.py
    Avançamos no projeto criando o vetor média e diminuindo cada imagem da média.

--- main.py
+++ main.py
@@ -9,3 +9,18 @@ def load_images(directory):
 	# load a greyscale version of each image
 	imgs = np.array([cv2.imread(i, 1).flatten() for i in jpgs])
 	return imgs
+
+#Carregamos as fotos em uma variável
+images = load_images(r"faces_jpeg")
+
+#Criando o vetor média
+mean = np.zeros(images[0].shape[0])
+for i in images:
+    mean = mean + i
+mean = mean * 1/100
+
+#Diminuímos cada coluna da matriz do vetor média
+images_transf = images
+for i in range(len(images)):
+    images_transf[i] = images[i] - mean
+
